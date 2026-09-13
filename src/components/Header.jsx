70bca0db0236f92578b82e2fb27628bd1d3e97ef
Nico Angelo  Garilva <nicogarilva@gmail.com>
2026-09-13 20:15:09 +0800

    opening the cart in the modal via new context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,20 @@ import { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
 import CartContext from "../store/CartContext";
+import { UserProgressContext } from "../store/UserProgressContext";
 
 export default function Header() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
   const totalItems = cartCtx.items.reduce(
     (total, item) => total + item.quantity,
     0,
   );
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
 
   return (
     <header id="main-header">
@@ -18,7 +25,9 @@ export default function Header() {
       </div>
 
       <nav>
-        <Button textOnly>Cart ({totalItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalItems})
+        </Button>
       </nav>
     </header>
   );
