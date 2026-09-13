6ef661f66932679fef7d5ee67aea7819eb0705df
Nico Angelo  Garilva <nicogarilva@gmail.com>
2026-09-13 18:09:21 +0800

    add Cart Context and UI buttons and modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,15 @@
+import { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
 
 export default function Header() {
+  const cartCtx = useContext(CartContext);
+  const totalItems = cartCtx.items.reduce(
+    (total, item) => total + item.quantity,
+    0,
+  );
+
   return (
     <header id="main-header">
       <div id="title">
@@ -9,7 +18,7 @@ export default function Header() {
       </div>
 
       <nav>
-        <button>Cart (0)</button>
+        <Button textOnly>Cart ({totalItems})</Button>
       </nav>
     </header>
   );
